c03c463e1e908fcb0c5b4b87f2c46671c828c35d
saragsz <saragilsanz207@gmail.com>
2026-02-04 18:09:13 +0100

    ejercicio 2 S2

---
+++ S02/fiboN.py
@@ -0,0 +1,12 @@
+def fibon(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        c = a + b
+        a = b
+        b = c
+    return a
+
+print("The 5th element is:" ,fibon(5))
+print("The 5th element is:" ,fibon(10))
+print("The 5th element is:" ,fibon(15))

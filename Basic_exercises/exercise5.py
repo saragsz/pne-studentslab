c0d7625d39a101913bd2ed06d92a10ca46b6be10
saragsz <saragilsanz207@gmail.com>
2026-02-11 18:28:08 +0100

    ejercicio 5

---
+++ Basic_exercises/exercise5.py
@@ -0,0 +1,15 @@
+#a
+words = ["Python", "is", "a", "programming", "language"]
+for i in words:
+    print(i ,"->" ,len(i),"characters")
+
+#b
+n = 1
+while n <= 1000:
+    print(n)
+    n = n * 2
+
+
+
+
+

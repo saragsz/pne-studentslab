7d18a9727df39a34674f4eb05da790a82dc8092c
saragsz <saragilsanz207@gmail.com>
2026-02-11 18:44:55 +0100

    ejercicio 6

--- Basic_exercises/exercise6.py
+++ Basic_exercises/exercise6.py
@@ -1,3 +1,4 @@
+#a
 def is_even(number):
     if (number % 2) == 0:
         result = "True"
@@ -5,9 +6,22 @@ def is_even(number):
         result = "False"
     return result
 
-
 print(is_even(4))
 print(is_even(7))
 print(is_even(0))
 print(is_even(-3))
 print(is_even(10))
+
+#b
+def classify_triangle(a, b, c):
+    if a == b == c:
+        result = "equilateral"
+    elif a == b or a == c or b == c:
+        result = "isosceles"
+    else:
+        result = "scalene"
+    return result
+
+print(classify_triangle(5, 5, 5))
+print(classify_triangle(3, 3, 4))
+print(classify_triangle(3, 4, 5))

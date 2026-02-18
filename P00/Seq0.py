f6acd29caa2a588a1976a483ca3c5a75bb1f8b5f
saragsz <saragilsanz207@gmail.com>
2026-02-18 18:43:30 +0100

    ejercicio 4

--- P00/Seq0.py
+++ P00/Seq0.py
@@ -12,3 +12,5 @@ def seq_len(seq):
     return len(seq)
 
 def seq_count_base(seq, base):
+    return seq.count(base)
+
---
+++ P00/e4.py
@@ -0,0 +1,6 @@
+from Seq0 import seq_read_fasta
+
+bases_list = ["A","C","T","G"]
+gene_list = ["U5", "ADA", "FRAT1", "FXN"]
+for gene in gene_list:
+    if

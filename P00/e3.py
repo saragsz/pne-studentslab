62e236b3eeba6008b494876c2a6e9f40f67fda96
saragsz <saragilsanz207@gmail.com>
2026-02-18 22:04:42 +0100

    ejercicio 3 y 4

--- P00/e3.py
+++ P00/e3.py
@@ -1,12 +1,13 @@
 from Seq0 import seq_read_fasta, seq_len
 
-gene_list = ["U5", "ADA", "FRAT1", "FXN"]
+if __name__ == "__main__":
+    gene_list = ["U5", "ADA", "FRAT1", "FXN"]
 
-for gene in gene_list:
-    ruta = "../S04/sequences/" + gene + ".txt"
-    g = seq_read_fasta(ruta)
+    for gene in gene_list:
+        ruta = "../S04/sequences/" + gene + ".txt"
+        g = seq_read_fasta(ruta)
 
-    print(gene, "Length:", seq_len(g))
+        print(gene, "Length:", seq_len(g))
 
 
 
--- P00/e4.py
+++ P00/e4.py
@@ -1,6 +1,13 @@
-from Seq0 import seq_read_fasta
+from Seq0 import seq_read_fasta,seq_count_base
 
-bases_list = ["A","C","T","G"]
-gene_list = ["U5", "ADA", "FRAT1", "FXN"]
-for gene in gene_list:
-    if
+if __name__ == "__main__":
+    bases_list = ["A","C","T","G"]
+    gene_list = ["U5", "ADA", "FRAT1", "FXN"]
+    for gene in gene_list:
+        ruta = "../S04/sequences/" + gene + ".txt"
+        seq = seq_read_fasta(ruta)
+        print ("Gene", gene + ":")
+
+        for b in bases_list:
+            count = seq_count_base(seq,b)
+            print("  ", b, ":", count)

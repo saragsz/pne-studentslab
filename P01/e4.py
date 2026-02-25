5d0f920e51f386616ba9456b98dfabd3f91048af
saragsz <saragilsanz207@gmail.com>
2026-02-25 18:18:57 +0100

    e4

---
+++ P01/e4.py
@@ -0,0 +1,9 @@
+from Seq1 import Seq
+
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+print(f"Sequence 1: (Lenght:{s1.len()}) {s1}")
+print(f"Sequence 1: (Lenght:{s2.len()}) {s2}")
+print(f"Sequence 1: (Lenght:{s3.len()}) {s3}")
